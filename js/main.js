31ea579467ece7dbf11442d04654be288ce90942
宗像勇樹 <munakatayuki@munekatayuukinoMacBook-Pro.local>
2021-08-22 11:22:51 +0900

    カードの取り合い処理実装

--- js/main.js
+++ js/main.js
@@ -37,103 +37,103 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //自分の手札生成
     const myTable = document.getElementById('my-table');
-    const myNums = [];
     for (let i = 0; i < 1; i++) {
         let tr = document.createElement('tr');
         for (let j = 0; j < 5; j++) {
             let td = document.createElement('td');
             let tempCard = '';
             tempCard = cards[j];
+            // console.log(;
             td.classList.add('card');
             td.onclick = flip;
             td.num = tempCard.num;
             td.style.backgroundImage = `url(images/${tempCard.front})`;
             // let num = \\``.tempCard.num;
-            td.setAttribute("id", `no-${tempCard.num}`);
-            myNums.push(tempCard.num);
+            // td.classList.add(`no-${tempCard.num}`);
+            td.classList.add('card', 'me',`no-${tempCard.num}`,'noTouch');
             tr.setAttribute("id", `my_tr`);
             tr.appendChild(td);
             myTable.appendChild(tr);
         }
     }
-    // console.log(cardss[0].querySelector("no8"));
-    // 要素の取得→配列に格納⇨配列の中身。重複削除？？
-    // 要素の取得→ループで削除？？
+
     //相手手札の生成
     const partnerTable = document.getElementById('partnerーtable');
-    const paNums = [];
     for (let i = 0; i < 1; i++) {
         let tr = document.createElement('tr');
-       
+
         for (let j = 24; j < 29; j++) {
             let td = document.createElement('td');
             //TODO::jだけで良くない？？
+
             let tempCard = cards[j];
-            td.classList.add('card', 'partner', 'back');
+            td.classList.add('card', 'partner',`no-${tempCard.num}`, 'back');
+
             td.onclick = flip;
             //以下を追加
             td.num = tempCard.num;
             td.style.backgroundImage = `url(images/${tempCard.front})`;
             //一致したものを削除できるようにカードの数字を付与
-            td.setAttribute("id", `no-${tempCard.num}`);
-            paNums.push(tempCard.num);
+            // td.classList.add();
+            tr.setAttribute("id", `pa_tr`);
             tr.appendChild(td);
             partnerTable.appendChild(tr);
         }
     }
 
-    function addNextButton(){
+    //相手のターン処理のためのボタン追加
+    function addNextButton() {
         const container = document.getElementById('container');
         let tees = document.createElement('button');
         tees.setAttribute("id", `next`);
-        container.insertBefore(tees,null);
+        container.insertBefore(tees, null);
         let text = document.createTextNode('次へ');
         //アンカータグにテキストノード追加
         tees.appendChild(text);
         const test = document.getElementById("next");
         test.addEventListener("click", event => {
-            
+
         });
     }
 
-    function removeDupCard(cardId) {
-        let cardss = document.querySelector(`#my-table #${cardId}`);
-        // let cardss2 = document.querySelector(`#my-table #${cardId}`);
-        // console.log(cardss2);
-        cardss.parentNode.removeChild(cardss);
-        // cardss.parentNode.removeChild(cardss2);
+    //自分のカードを削除
+    function removeDupCard(cardClass, parentId) {
+        console.log(parentId);
+
+        //1回目で相手から取ったカードを。２回目で自分のカードを削除する。
+        for (let i = 0; i <= 1; i++) {
+            let cardss = document.querySelector(`#${parentId} .${cardClass[2]}`);
+            cardss.parentNode.removeChild(cardss);
+        }
     }
 
-    // removeDupCard('partnerーtable',paNums);
-    // removeDupCard('myーtable',myNums);
 
     //指定のカードを追加する処理
-    function getCard(card) {
+    function getCard(card, trId) {
         //https://itsakura.com/js-queryselectorall
         //https://www.javadrive.jp/javascript/dom/index28.html
         let style = getComputedStyle(card.target);
         let imageUrl = style.getPropertyValue('background-image');
         card.target.remove();
         // 手札の数をカウント、取得したカードの配置場所を決めるため
-        myCardCount = document.querySelectorAll('#my_tr .card').length;
+        // myCardCount = document.querySelectorAll('#my_tr .card').length;
 
         //クラス、ID付与
         let td = document.createElement('td');
-        td.classList.add('card');
+        let class_name = card.target.className.split(' ');
+        td.classList.add('card', class_name[2]);
+        // td.setAttribute("class", `card`);
         // td.setAttribute("id",`my_card.${myCardCount}`);
         td.style.backgroundImage = imageUrl;
 
-        //自分のテーブルに追加
-        let parentDiv2 = document.getElementById("my_tr");
-        let myCard = document.getElementById(`my_card.${myCardCount}`);
-
-        // 追加
-        // https://js.studio-kingdom.com/javascript/node/insert_before
-        insertedElement = parentDiv2.insertBefore(td, myCard);
-        insertedElement.setAttribute("id", `${card.target.id}`);
-
+        let parentDiv2  = document.getElementById('pa_tr');;
+        if(trId == 'pa_tr'){
+            //カードを引いた人のテーブルに追加
+            parentDiv2 = document.getElementById('my_tr');
+        }
+        insertedElement = parentDiv2.insertBefore(td, null);
+        return parentDiv2.id;
         //重複チェック自分のと、相手のを削除。。
-
     }
 
     //以下の変数を追加
@@ -141,22 +141,27 @@ window.addEventListener('DOMContentLoaded', function () {
     let flipTimerId = NaN;
 
     function flip(cards) {
-        let td = cards.target;
 
+        let td = cards.target;
+        let tdParent = document.getElementsByClassName(td.className)[0];
+        let trParent = tdParent.parentNode;
         //表にしたカードを取得、削除する
-        if (td.classList.contains('back')) {
+        if (td.classList.contains('back')||td.classList.contains('noTouch')) {
             td.classList.add('open');
-        }else{
-            getCard(cards);
-            removeDupCard(cards.target.id);
-            removeDupCard(cards.target.id);
+        } else {
+            
+            let getId = getCard(cards, trParent.id);
+
+            console.log(trParent);
+            removeDupCard(cards.target.className.split(' '), getId);
             addNextButton();
         }
 
         td.classList.remove('back');//カードを表にする。
+        td.classList.remove('noTouch');//カードを表にする。
 
         // if (!td.classList.contains('back') || flipTimerId) {
-        console.log("クリックされました");
+
     }
 
 
